74f03efaea3b960ebf0ab3a3369a909e75b3d249
Kamil Górski <92941265+Kami9137@users.noreply.github.com>
2021-11-27 19:36:00 +0100

    Add files via upload

---
+++ zad5.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, liczba, suma = 0;
+    cout << "Podaj n: ", cin >> n;
+
+    if (n > 0) {
+        for (int i = 0; i < n; i++) {
+            cout << "Podaj liczbe: ", cin >> liczba;
+            if (liczba % 2 == 0)
+                suma += liczba;
+        }
+    }
+    else {
+        cout << "Ilosc wyrazow w ciagu nie moze byc <=0\n";
+        cout << "Podaj ponownie n: ", cin >> n;
+        for (int i = 0; i < n; i++) {
+            cout << "Podaj liczbe: ", cin >> liczba;
+            if (liczba % 2 == 0)
+                suma += liczba;
+        }
+    }
+    cout << "Suma liczb parzystych: " << suma << endl;
+}
