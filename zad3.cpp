97c7a8381331877adf898fcdd6b350ab630813b8
Kamil Górski <92941265+Kami9137@users.noreply.github.com>
2021-11-27 19:31:20 +0100

    Add files via upload

---
+++ zad3.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, temp;
+	float pkt, suma = 0;
+
+	cout << "Podaj ilosc studentow: ", cin >> n;
+		temp = n;
+
+		while (1) {
+			{
+				cout << "Podaj liczbe punktow zdobytych przez studenta: \n", cin >> pkt;
+				if (pkt >= 0 && pkt <= 100) {
+					suma += pkt;
+					n--;
+				}
+				else continue;
+				if (n == 0) break;
+			}
+		}
+		cout << "Suma punktow: " << suma << endl;
+		cout << "Srednia arytmetyczna zdobytych punktow wynosi: " << suma / temp << endl;
+	}
